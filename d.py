ee96dce7214c1de4d24fd2da8a72c7985740f64a
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 21:48:12 +0545

    Update d.py

--- d.py
+++ d.py
@@ -15,24 +15,26 @@ def get_proxies(url):
         print(f"Failed to retrieve proxies: {e}")
         return []
 
+# Function to visit URL using proxy
+def visit_url_with_proxy(url, proxy):
+    try:
+        response = requests.get(url, proxies={'http': proxy})
+        if response.status_code == 200:
+            print(f"Success: Visited {url} using proxy {proxy}")
+        else:
+            print(f"Error: Unable to visit {url} using proxy {proxy}. Status Code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
 # Main function
 def main():
     url = 'https://10.alabbd.xyz/'
     proxy_url = 'https://raw.githubusercontent.com/Ramxantanha/data/main/proxies.txt'
     proxies = get_proxies(proxy_url)
-    num_iterations = 100
 
-    for i in range(num_iterations):
-        for proxy in proxies:
-            try:
-                response = requests.get(url, proxies={'http': proxy})
-                if response.status_code == 200:
-                    print(f"Iteration {i+1}: Success: Visited {url} using proxy {proxy}")
-                else:
-                    print(f"Iteration {i+1}: Error: Unable to visit {url} using proxy {proxy}. Status Code: {response.status_code}")
-            except requests.exceptions.RequestException as e:
-                print(f"Iteration {i+1}: Error: {e}")
-        time.sleep(0.01)  # Wait for 1 second before making the next request
+    for proxy in proxies:
+        visit_url_with_proxy(url, proxy)
+        time.sleep(1)  # Wait for 1 second before visiting the next proxy
 
 if __name__ == "__main__":
     main()
