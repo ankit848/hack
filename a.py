76536fdaf75e3b580141930a537df550bf568e28
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 17:31:54 +0545

    Update a.py

--- a.py
+++ a.py
@@ -7,6 +7,13 @@ def login_to_moneypluslife(id, password):
         'p1': password,
         '1d2es5e8s': ''  # Add the missing value for the form field
     }
+      headers = {
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "*/*",
+    "Origin": "https://moneyplus.info",
+    "Referer": "https://moneypluslife.info/panel/login",  # Corrected referer URL
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
+}
 
     # Define the URL for form submission
     login_url = 'https://moneypluslife.info/panel/Authentic'
@@ -14,7 +21,7 @@ def login_to_moneypluslife(id, password):
     # Create a session
     with requests.Session() as session:
         # Send POST request to login
-        response = session.post(login_url, data=form_data)
+        response = session.post(login_url, data=form_data, headers=headers)
 
         # Check if the session cookie is set
         if 'session' in session.cookies:
