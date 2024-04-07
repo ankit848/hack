dc471f86bea30e96b887a591a83526bab3a45c4d
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 15:11:39 +0545

    Update a.py

--- a.py
+++ a.py
@@ -1,58 +1,68 @@
-import requests,uuid
+import requests
+import uuid
+import random
+import os
 import time
 import json
-session=requests.Session()
-uid="shresthaankit444@gmail.com"
-ps="rakesh@1"
+import string  # Add import for string module
+
+session = requests.Session()
+uid = "shresthaankit444@gmail.com"
+ps = "rakesh@1"
+
+def one():
+    ua = 'Mozilla/5.0 (Linux; Android 12; 22011119UY Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.154 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]'
+    return ua
 
 try:
-    user_agent="Dalvik/2.1.0 (Linux; U; Android 9; moto e6 Build/PCB29.73-65-3) [FBAN/Orca-Android;FBAV/235.1.0.9.122;FBPN/com.facebook.orca;FBLC/en_US;FBBV/175782189;FBCR/Metro by T-Mobile;FBMF/motorola;FBBD/motorola;FBDV/moto e6;FBSV/9;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1344};FB_FW/1;]"
-            data = {
-            "adid": str(uuid.uuid4()),
-            "format": "json",
-            "device_id": str(uuid.uuid4()),
-            "cpl": "true",
-            "family_device_id": str(uuid.uuid4()),
-            "credentials_type": "device_based_login_password",
-            "error_detail_type": "button_with_disabled",
-            "source": "device_based_login",
-            "email": uid,
-            "password": ps,
-            "access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
-            "generate_session_cookies": "1",
-            "meta_inf_fbmeta": "",
-            "advertiser_id": str(uuid.uuid4()),
-            "currently_logged_in_userid": "0",
-            "locale": "en_GB",
-            "client_country_code": "GB",
-            "method": "auth.login",
-            "fb_api_req_friendly_name": "authenticate",
-            "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
-            "api_key": "882a8490361da98702bf97a021ddc14d"}
-            headers = {
-            'User-Agent': user_agent,
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Host': 'graph.facebook.com',
-            'X-FB-Net-HNI': str(random.randint(20000, 40000)),
-            'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
-            'X-FB-Connection-Type': 'MOBILE.LTE',
-            'X-Tigon-Is-Retry': 'False',
-            'X-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
-            'X-fb-device-group': '5120',
-            'X-FB-Friendly-Name': 'ViewerReactionsMutation',
-            'X-FB-Request-Analytics-Tags': 'graphservice',
-            'X-FB-HTTP-Engine': 'Liger',
-            'X-FB-Client-IP': 'True',
-            'X-FB-Server-Cluster': 'True',
-            'X-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
-            p = requests.post("https://b-graph.facebook.com/auth/login",data=data,headers=headers,allow_redirects=False).text
-            q=json.loads(p)
-            if "session_key" in q:
-               print('sakes')
-               break
-    else:
-                print('haha')
-    except:
-        time.sleep(4)
+    fbav = f'{random.randint(111, 999)}.0.0.{random.randint(11, 99)}.{random.randint(111, 999)}'
+    fbbv = str(random.randint(111111111, 999999999))
+    android_version = '12'  # Assuming these values, you should replace them with actual values.
+    model = 'YourModel'
+    build = 'YourBuild'
+    fblc = 'YourFblc'
+    fbcr = 'YourFbcr'
+    fbmf = 'YourFbmf'
+    fbbd = 'YourFbbd'
+    fbdv = 'YourFbdv'
+    fbsv = 'YourFbsv'
+    fbca = 'YourFbca'
+    fbdm = 'YourFbdm'
 
- 
+    data = {
+        "adid": str(''.join(random.choices(string.hexdigits, k=16))),  # Fix typo here, it should be `choices` instead of `choices`.
+        "format": "json",
+        "device_id": str(uuid.uuid4()),
+        "email": uid,
+        "password": ps,
+        "generate_analytics_claims": "1",
+        "credentials_type": "password",
+        "source": "login",
+        "error_detail_type": "button_with_disabled",
+        "enroll_misauth": "false",
+        "generate_session_cookies": "1",
+        "generate_machine_id": "1",
+        "fb_api_req_friendly_name": "authenticate",
+    }
+    headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "User-Agent": one(),  # Fix function call, it should be `one()` instead of `ONE()`.
+        "Authorization": "OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32",
+        "X-FB-Friendly-Name": "authenticate",
+        "X-FB-Connection-Type": "unknown",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-FB-HTTP-Engine": "Liger",
+        "Content-Length": "329",
+    }
+    url = 'https://b-graph.facebook.com/auth/login'
+    po = session.post(url, data=data, headers=headers).json()
+    if 'session_key' in po:
+        print("Success")
+    else:
+        print('Error')
+except requests.exceptions.ConnectionError:
+    time.sleep(20)
+except Exception as e:
+    pass
