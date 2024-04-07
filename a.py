9d3f438bea669d3402f71ad147621275b29dea81
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 14:38:38 +0545

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,5 @@
 import requests
-
+session = requests.Session()
 id = "MPL2298238"
 pas = "9807298843"
 url = 'https://moneypluslife.info/panel/Login'
@@ -14,7 +14,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]"
 }
 
-response = requests.get(url, data=data, headers=headers)
+response = requests.post(url, data=data, headers=headers)
 po = response.json()
 
 if 'session_key' in po:
