9105848b26747683e3478e47cbf4b6dca2b7fd86
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 16:27:19 +0545

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 import requests
 import json
+
 session = requests.Session()
 id = "MPL2298238"
 pas = "9807298843"
@@ -14,6 +15,13 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]"
 }
 
-response = session.post(url, data=data, headers=headers)
-so=response.json()
-print(so)
+try:
+    response = session.post(url, data=data, headers=headers)
+    response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+    try:
+        json_response = response.json()
+        print(json_response)
+    except json.JSONDecodeError:
+        print("Response is not in JSON format:", response.text)
+except requests.RequestException as e:
+    print("An error occurred:", e)
