54b893f3f6e9c7a85910c32be8e99e2002def8f2
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 14:26:46 +0545

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import requests
 
-id = "1010201"
-pas = "pppos"
+id = "MPL2298238"
+pas = "9807298843"
 url = 'https://moneypluslife.info/panel/Login'
 data = {
     "u1": id,
@@ -10,6 +10,7 @@ data = {
 headers = {
     "Accept-Encoding": "gzip, deflate",
     "Accept": "*/*",
+     "path":"/panel/Authentic",
     "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]"
 }
 
@@ -20,3 +21,4 @@ if 'session_key' in po:
     print('Logged in successfully.')
 else:
     print("Login failed.")
+
