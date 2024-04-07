1a333c1c601abda1ac7e4e4a6052f27c7496e8cb
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-07 17:20:53 +0545

    Update a.py

--- a.py
+++ a.py
@@ -8,13 +8,15 @@ def login_to_moneypluslife(id, password):
     }
 
     # Define the URL for form submission
-    login_url = 'https://moneypluslife.info/panel/Login'
+    login_url = 'https://moneypluslife.info/panel/Authentic'
 
     # Create a session
     with requests.Session() as session:
         # Send POST request to login
         response = session.post(login_url, data=form_data)
-
+      print("Response content:", response.content)
+print(response)
+"""
         # Check if the session cookie is set
         if 'session' in session.cookies:
             print("Login Successful!")
@@ -23,7 +25,7 @@ def login_to_moneypluslife(id, password):
         else:
             print("Login Unsuccessful!")
              print("Response content:", response.content)
-
+"""
 if __name__ == "__main__":
     # Replace 'your_id' and 'your_password' with the actual credentials
     id = 'MPL2298238'
