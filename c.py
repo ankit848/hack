f718cf6cc16cdbbcf8898c97a0926c132cf43c32
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-20 08:43:30 +0545

    Update c.py

--- c.py
+++ c.py
@@ -8,14 +8,17 @@ def generate_random_numbers():
 
 # Function to get token from server
 def get_token():
-    url = 'https://10.alabbd.xyz/user/register'
+    url = 'https://loyality-one.site/user/register'
     headers = {
         "Accept-Encoding": "gzip, deflate, br",
         "Accept": "*/*",
-        "Host":"10.alabbd.xyz",
-        "Referer":"https://10.alabbd.xyz/user/login",
+        "Host":"loyality-one.site",
+        "Referer":"https://loyality-one.site/user/login",
         "Sec-Fetch-User":"1",
         "Upgrade-Insecure-Requests":"1",
+        "Set-Cookie":"XSRF-TOKEN=eyJpdiI6IllQaVNnYXdzQ09OalIxQzhGMlI0eWc9PSIsInZhbHVlIjoicnpBYzlLeXVjeldMYjIyMmdDajdkVzR1NG1rTHRYaWhGdHdFTDlwTkFMMnZsYzUydkw4VEFIdm5USkZKVDVxWlNTVWFmSzNoUTZMNllOdnc2VFVOYXNWWVBXMVhJZDg0KzZwdFRIZWVWZ3VJeUs2THFTYjI0aXR6RXpqbTFDa2IiLCJtYWMiOiJhZDliNGU4MmM4YzgzMmViOGIzMjRjMjkxNmUwZTkwYWFjNDg4NzYwZDNlNjM2MjMzYTczNTY2MjgyMmU3N2I5IiwidGFnIjoiIn0%3D; expires=Sat, 20 Apr 2024 04:48:24 GMT; Max-Age=7200; path=/; samesite=lax; secure",
+        "Set-Cookie":"hyiplab_session=eyJpdiI6InhGZzBHc3dha3BNcWlVMlF3ZlJLOWc9PSIsInZhbHVlIjoiS0VNVC9IS3dLVDRsdmlhaUNncWJTbUsxNmZmNEZNaHowK2wzdmllRVhId3RFbDR0WUJ1dVZtU1JYM1dhUFUzelo3WHVMU0lnOU1mWUQxOE1ackppcjFvR2NBb2RnVGtuSHdoT0c5Sy9qYktCT1JEZlljUGoxUFNrMGxSa3V2aXAiLCJtYWMiOiI1ZDY0YzViMDdjMWU1NTE3Y2I4MmYwYTM5NTVjMzVjMzAxMWQxMzI2YWZkYzhlYTU3NjFkOTUxOTcwZTNhOGZlIiwidGFnIjoiIn0%3D; expires=Sat, 20 Apr 2024 04:48:24 GMT; Max-Age=7200; path=/; httponly; samesite=lax; secure",
+        "Cookie":"MicrosoftApplicationsTelemetryDeviceId=58f4586a-8ed6-4cc7-8e4c-4044676e6428; MicrosoftApplicationsTelemetryFirstLaunchTime=2024-04-20T02:44:43.485Z; MicrosoftApplicationsTelemetryDeviceId=58f4586a-8ed6-4cc7-8e4c-4044676e6428; MicrosoftApplicationsTelemetryFirstLaunchTime=2024-04-20T02:44:43.485Z; XSRF-TOKEN=eyJpdiI6ImFZaVcrN25YNnhnMm5HbERna3NzR2c9PSIsInZhbHVlIjoiSHM3cExjdE1rcFp1ekpsWVh2T3RyVDF3emhGUDZ1ZDdlRzhydTJFNjl3NWlTWisvL1JSOWNoU2FUMkZyNTIvc3VBQzJjV1ZJQVJ2T05zTGlQWCtvNmdXTkY0eUR0N1hZdmUydVFFM0RZMm1PMVJoVExSSE1oRXZFMkJYZXhlRGEiLCJtYWMiOiI5YThiMTUwZjI2ODk1MTkzMGRhMzljOGRhMTgxZTYzYjliNTNjMDJlMThlYmMzNmYyYjk0NTFhODI4ZjE2YTE1IiwidGFnIjoiIn0%3D; hyiplab_session=eyJpdiI6ImxOUFl2RWg1U0hBa0FQMXBMNzFjNmc9PSIsInZhbHVlIjoicTRKb3J0U2tMZEg2TUFmYzZVNWFveERIMXFUSUxQN2Z1cGZjNHl3STFMT1NMRjRNU1lIem85OVc1b0VLU25lUEFtWVYzV09uZzVJczhMQ0VtTXNTQ2RnK20yU2hXa2duNE5pR290ZktlMEYwRVhQNHpQNnRESVRia0MzNGdKc3kiLCJtYWMiOiI0Yjg3Y2VhMzk3MmExMWRhY2JmMGE3YmNlZDgwODgyN2RkZDgzMjZjNDZkZTI2NDAxMzFhMDNjMDdkNmFjZjgyIiwidGFnIjoiIn0%3D",
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
     }
     try:
@@ -31,29 +34,28 @@ def get_token():
         return None
 _token = get_token()
 # Function to submit form data
-def submit_form(_token,firstname, lastname, username, email, country, mobile_code, country_code, mobile, password, password_confirmation):
+def submit_form(_token,areacode, username, email, password, password_confirmation):
 
-    url = 'https://10.alabbd.xyz/user/register'
+  url = 'https://loyality-one.site/user/register'
     data = {
         '_token': _token,
-        'firstname': firstname,
-        'lastname': lastname,
-        'username': username,
+        'username': mobile,
         'email': email,
-        'mobile': mobile,
         'password': password,
         'password_confirmation': password_confirmation,
         'country': country,
-        'mobile_code': mobile_code,
-        'country_code': country_code
+        'area_code': mobile_code,
     }
     headers = {
-       "Accept-Encoding": "gzip, deflate, br",
+           "Accept-Encoding": "gzip, deflate, br",
         "Accept": "*/*",
-        "Host":"10.alabbd.xyz",
-        "Referer":"https://10.alabbd.xyz/user/login",
+        "Host":"loyality-one.site",
+        "Referer":"https://loyality-one.site/user/login",
         "Sec-Fetch-User":"1",
         "Upgrade-Insecure-Requests":"1",
+        "Set-Cookie":"XSRF-TOKEN=eyJpdiI6IllQaVNnYXdzQ09OalIxQzhGMlI0eWc9PSIsInZhbHVlIjoicnpBYzlLeXVjeldMYjIyMmdDajdkVzR1NG1rTHRYaWhGdHdFTDlwTkFMMnZsYzUydkw4VEFIdm5USkZKVDVxWlNTVWFmSzNoUTZMNllOdnc2VFVOYXNWWVBXMVhJZDg0KzZwdFRIZWVWZ3VJeUs2THFTYjI0aXR6RXpqbTFDa2IiLCJtYWMiOiJhZDliNGU4MmM4YzgzMmViOGIzMjRjMjkxNmUwZTkwYWFjNDg4NzYwZDNlNjM2MjMzYTczNTY2MjgyMmU3N2I5IiwidGFnIjoiIn0%3D; expires=Sat, 20 Apr 2024 04:48:24 GMT; Max-Age=7200; path=/; samesite=lax; secure",
+        "Set-Cookie":"hyiplab_session=eyJpdiI6InhGZzBHc3dha3BNcWlVMlF3ZlJLOWc9PSIsInZhbHVlIjoiS0VNVC9IS3dLVDRsdmlhaUNncWJTbUsxNmZmNEZNaHowK2wzdmllRVhId3RFbDR0WUJ1dVZtU1JYM1dhUFUzelo3WHVMU0lnOU1mWUQxOE1ackppcjFvR2NBb2RnVGtuSHdoT0c5Sy9qYktCT1JEZlljUGoxUFNrMGxSa3V2aXAiLCJtYWMiOiI1ZDY0YzViMDdjMWU1NTE3Y2I4MmYwYTM5NTVjMzVjMzAxMWQxMzI2YWZkYzhlYTU3NjFkOTUxOTcwZTNhOGZlIiwidGFnIjoiIn0%3D; expires=Sat, 20 Apr 2024 04:48:24 GMT; Max-Age=7200; path=/; httponly; samesite=lax; secure",
+        "Cookie":"MicrosoftApplicationsTelemetryDeviceId=58f4586a-8ed6-4cc7-8e4c-4044676e6428; MicrosoftApplicationsTelemetryFirstLaunchTime=2024-04-20T02:44:43.485Z; MicrosoftApplicationsTelemetryDeviceId=58f4586a-8ed6-4cc7-8e4c-4044676e6428; MicrosoftApplicationsTelemetryFirstLaunchTime=2024-04-20T02:44:43.485Z; XSRF-TOKEN=eyJpdiI6ImFZaVcrN25YNnhnMm5HbERna3NzR2c9PSIsInZhbHVlIjoiSHM3cExjdE1rcFp1ekpsWVh2T3RyVDF3emhGUDZ1ZDdlRzhydTJFNjl3NWlTWisvL1JSOWNoU2FUMkZyNTIvc3VBQzJjV1ZJQVJ2T05zTGlQWCtvNmdXTkY0eUR0N1hZdmUydVFFM0RZMm1PMVJoVExSSE1oRXZFMkJYZXhlRGEiLCJtYWMiOiI5YThiMTUwZjI2ODk1MTkzMGRhMzljOGRhMTgxZTYzYjliNTNjMDJlMThlYmMzNmYyYjk0NTFhODI4ZjE2YTE1IiwidGFnIjoiIn0%3D; hyiplab_session=eyJpdiI6ImxOUFl2RWg1U0hBa0FQMXBMNzFjNmc9PSIsInZhbHVlIjoicTRKb3J0U2tMZEg2TUFmYzZVNWFveERIMXFUSUxQN2Z1cGZjNHl3STFMT1NMRjRNU1lIem85OVc1b0VLU25lUEFtWVYzV09uZzVJczhMQ0VtTXNTQ2RnK20yU2hXa2duNE5pR290ZktlMEYwRVhQNHpQNnRESVRia0MzNGdKc3kiLCJtYWMiOiI0Yjg3Y2VhMzk3MmExMWRhY2JmMGE3YmNlZDgwODgyN2RkZDgzMjZjNDZkZTI2NDAxMzFhMDNjMDdkNmFjZjgyIiwidGFnIjoiIn0%3D",
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
    }
     try:
@@ -61,14 +63,9 @@ def submit_form(_token,firstname, lastname, username, email, country, mobile_cod
         response.raise_for_status()  # Raise an exception for non-2xx status codes
         print("Form submitted successfully.")
         print("Submitted Data:")
-        print(f"First Name: {firstname}")
-        print(f"Last Name: {lastname}")
-        print(f"Username: {username}")
+        print(f"Username: {mobile}")
         print(f"Email: {email}")
-        print(f"Mobile: {mobile}")
-        print(f"Country: {country}")
         print(f"Mobile Code: {mobile_code}")
-        print(f"Country Code: {country_code}")
         print(f"Password: {password}")
         print(f"Password Confirmation: {password_confirmation}")
         print(f"Response URL: {response.url}")
@@ -77,13 +74,10 @@ def submit_form(_token,firstname, lastname, username, email, country, mobile_cod
 
 # Main function
 def main():
-    firstname = 'Hacker'
-    lastname = 'King'
     password = 'hacker@12345'
     password_confirmation = 'hacker@12345'
-    country = 'Nepal'
-    mobile_code = '977'
-    country_code = 'NP'
+    mobile_code = '+977'
+ 
 
     for _ in range(2):  # Loop 2 times
         mobile_base = '9800005678'
