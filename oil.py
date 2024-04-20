d165f954c7fc68247a2ac2a0ba5165a25417f919
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-20 12:28:16 +0545

    Update oil.py

--- oil.py
+++ oil.py
@@ -1,6 +1,6 @@
 import requests
 import random
-import hashlib
+from bs4 import BeautifulSoup
 
 # Function to generate random numbers
 def generate_random_numbers():
@@ -8,11 +8,11 @@ def generate_random_numbers():
 
 # Function to hash the password
 def hash_password(password):
-    hashed_pass = hashlib.sha256(password.encode()).hexdigest()
-    return hashed_pass
+    # Add your password hashing logic here
+    return password
 
-# Function to submit form data
-def submit_form(account, code, pwd, user_type):
+# Function to submit form
+def submit_form(session, account, code, pwd, user_type):
     url = 'https://oli-bp.com/#/register'
     hashed_pass = hash_password(pwd)  # Hash the password
     data = {
@@ -24,7 +24,7 @@ def submit_form(account, code, pwd, user_type):
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
     }
-    response = requests.post(url, data=data, headers=headers)
+    response = session.post(url, data=data, headers=headers)
     print(pwd)
     print(account)
     print(response.url)
@@ -33,14 +33,14 @@ def submit_form(account, code, pwd, user_type):
     else:
         print("Form submission failed.")
 
-# Main function
 def main():
-    code = '384251'
+    code = '517651'
     pwd = 'Hacker123@'
     user_type = 1
     for _ in range(5):  # Loop 5 times
         account = f'hackers{generate_random_numbers()}p{generate_random_numbers()}@gmail.com'
-        submit_form(account, code, pwd, user_type)
+        with requests.Session() as session:
+            submit_form(session, account, code, pwd, user_type)
 
 if __name__ == "__main__":
     main()
