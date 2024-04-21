3c296c681074079fbed4eb64663974b31d69295c
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-21 15:02:10 +0545

    Update my.py

--- my.py
+++ my.py
@@ -30,7 +30,7 @@ def submit_form(name, email, pass):
 
 # Main function
 def main():
-    password = 'ankit'
+    pass = 'ankit'
     for _ in range(3):  # Loop 3 times
         name = 'krishna'
         email = f'dinesh{generate_random_numbers()}p{generate_random_numbers()}@gmail.com'
