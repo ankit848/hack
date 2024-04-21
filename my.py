e058f60146dfd80fc54e26e9588238ef447fa3c5
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-21 14:32:45 +0545

    Create my.py

---
+++ my.py
@@ -0,0 +1,41 @@
+import requests
+import random
+
+# Function to generate random numbers
+def generate_random_numbers():
+    return ''.join(str(random.randint(0, 9)) for _ in range(3))
+
+# Function to submit form data
+def submit_form(name, email, password):
+    url = 'http://hacking122.rf.gd/login.php'
+    data = {
+        'name': uname,
+        'email': uemail,
+        'password': password,
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
+    }
+    response = requests.post(url, data=data, headers=headers)
+    print(email)
+    print(response.url)
+    if 'http://hacking122.rf.gd/backened.php' in response.url:
+        print("Form submitted successfully.")
+    else:
+        print("Form submission failed.")
+
+# Main function
+def main():
+    password = '1234567890'
+
+    for _ in range(55):  # Loop 3 times
+        uname = 'Radhey Radhey'
+        umobile = '9800005678'
+        umobile = int(umobile)  # Convert umobile back to an integer
+        umobile += random.randint(100, 999)  # Add a random 3-digit number to umobile
+        email = f'dinesh{generate_random_numbers()}p{generate_random_numbers()}@gmail.com'
+            
+        submit_form(name, email, password)
+
+if __name__ == "__main__":
+    main()
