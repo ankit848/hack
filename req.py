e0195a23e2bd4b22d34da21a0a825709e2f1e2a8
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-21 21:17:08 +0545

    Update req.py

--- req.py
+++ req.py
@@ -12,8 +12,7 @@ def send_request(url):
     global success_counter
     
     try:
-        # Define a list of User-Agent strings
-        user_agents = [
+       
                user_agents = [
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
