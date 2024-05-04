d54f3d9fc3f0cd88c916c98a0fbfccafc57ca885
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-05-04 20:25:18 +0545

    Update eop.py

--- eop.py
+++ eop.py
@@ -7,15 +7,15 @@ def generate_random_numbers():
 
 # Function to submit form data
 def submit_form(emtoken, user_email, user_pass, user_conpass, terms_check, user_ref):
-      url = 'https://earnzop.com/register/'
-  
+    url = 'https://earnzop.com/register/'
+
     data = {
         'emtoken': emtoken,
-        'user_name': user_name,
+        'user_name': generate_random_numbers(),  # Generate a random username
         'user_email': user_email,
         'user_pass': user_pass,
         'user_conpass': user_conpass,
-        'terms_check': term_check,
+        'terms_check': terms_check,
         'user_ref': user_ref
     }
 
@@ -23,7 +23,7 @@ def submit_form(emtoken, user_email, user_pass, user_conpass, terms_check, user_
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
     }
     response = requests.post(url, data=data, headers=headers)
-    print(user_name)
+    print(data['user_name'])
     print(response.url)
     if 'https://earnzop.com/dashboard/' in response.url:
         print("Form submitted successfully.")
@@ -32,12 +32,15 @@ def submit_form(emtoken, user_email, user_pass, user_conpass, terms_check, user_
 
 # Main function
 def main():
-        for _ in range(2):
-            emtoken = 'e5b431e58da1f00b8fe8b04c60f73f1d7a36d28d61b1d8c974ebcb50d56ac02d'
-            user_pass = 'hacker@1234a55'
-            user_conpass = 'hacker@1234a55'
-            terms_check = 'on'  # Define area_code within the main function
-            user_ref = 'AB1BB'
-            submit_form(emtoken, user_email, user_pass, user_conpass, terms_check, user_ref)
-            if __name__ == "__main__":
-                main()
+    for _ in range(2):
+        emtoken = 'e5b431e58da1f00b8fe8b04c60f73f1d7a36d28d61b1d8c974ebcb50d56ac02d'
+        user_email = 'example@example.com'  # Provide a valid email here
+        user_pass = 'hacker@1234a55'
+        user_conpass = 'hacker@1234a55'
+        terms_check = 'on'  # Define terms_check within the main function
+        user_ref = 'AB1BB'
+        submit_form(emtoken, user_email, user_pass, user_conpass, terms_check, user_ref)
+
+# Execute main function if the script is run directly
+if __name__ == "__main__":
+    main()
