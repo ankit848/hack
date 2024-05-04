e0af65f6ab5b7fc163afb157abf3f435ee9de170
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-05-04 20:13:28 +0545

    Update eop.py

--- eop.py
+++ eop.py
@@ -9,7 +9,7 @@ def generate_random_numbers():
 
 # Function to get token from server
 def get_token():
-    url = 'https://loyality-one.site/user/register'
+    url = 'https://earnzop.com/register/'
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; CLT-L29 Build/HUAWEICLT-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/405.0.0.23.72;]",
     }
@@ -18,23 +18,24 @@ def get_token():
             response = session.get(url, headers=headers)
             response.raise_for_status()  # Raise an exception for non-2xx status codes
             soup = BeautifulSoup(response.content, 'html.parser')
-            token = soup.find('input', {'name': '_token'}).get('value')
+            token = soup.find('input', {'name': 'emtoken'}).get('value')
             return token, session  # Return both token and session
         
     except requests.exceptions.RequestException as e:
         print(f"Failed to get token: {e}")
         return None, None
 
-def submit_form(_token, session, area_code, mobile, email, password, password_confirmation):
-    url = 'https://loyality-one.site/user/register'
+def submit_form(emtoken, session, user_email, user_pass, user_conpass, terms_check, user_ref):
+    url = 'https://earnzop.com/register/'
   
     data = {
-        '_token': _token,
-        'username': mobile,
-        'email': email,
-        'password': password,
-        'password_confirmation': password_confirmation,
-        'area_code': area_code,
+        'emtoken': emtoken,
+        'user_name': user_name,
+        'user_email': user_email,
+        'user_pass': user_pass,
+        'user_conpass': user_conpass,
+        'terms_check': term_check,
+        'user_ref': user_ref
     }
 
     headers = {
@@ -43,32 +44,30 @@ def submit_form(_token, session, area_code, mobile, email, password, password_co
 
     try:
         response = session.post(url, data=data, headers=headers)
-        if 'https://loyality-one.site/user/dashboard' in response.url:
-            print(f"Form submitted successfully for {mobile}.")
+        if 'https://earnzop.com/dashboard/' in response.url:
+            print(f"Form submitted successfully for {user_name}.")
         else:
-            print(f"Form submission failed for {mobile}.")
+            print(f"Form submission failed for {user_name}.")
     except Exception as e:
-        print(f"An error occurred for {mobile}: {e}")
+        print(f"An error occurred for {user_name}: {e}")
 
 def main():
-    password = 'hacker@1234a55'
-    password_confirmation = 'hacker@1234a55'
-    area_code = '+977'  # Define area_code within the main function
-    
-    num_workers = 2000 # Number of workers
+    user_pass = 'hacker@1234a55'
+    user_conpass = 'hacker@1234a55'
+    terms_check = 'ON'  # Define area_code within the main function
+    user_ref = 'AB1BB'
+    num_workers = 2 # Number of workers
     tokens_and_sessions = [get_token() for _ in range(num_workers)]  # Generate tokens and sessions for each worker
 
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
-        for token, session in tokens_and_sessions:
-            if token:
-                print("Token obtained successfully:", token)
+        for emtoken, session in tokens_and_sessions:
+            if emtoken:
+                print("Token obtained successfully:", emtoken)
                 for _ in range(1):  # Loop 3 times for each worker
-                    mobile_base = '9700000678'
-                    mobile = int(mobile_base) + random.randint(10, 99999)  # Add a random 4-digit number to mobile_base
-                    usernames = f'hayackerss{generate_random_numbers()}'
-                    email = f'{usernames}p{generate_random_numbers()}@gmail.com'
+                    user_name = f'hayackerss{generate_random_numbers()}'
+                    user_email = f'{usernames}p{generate_random_numbers()}@gmail.com'
                     
-                    executor.submit(submit_form, token, session, area_code, mobile, email, password, password_confirmation)
+                    executor.submit(submit_form, emtoken, session, user_email, user_pass, user_conpass, terms_check, user_ref)
             else:
                 print("Failed to obtain token for worker.")
 
