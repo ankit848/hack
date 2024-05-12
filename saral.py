24ee286478bbcffd1538401f706dceb51c6489a3
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-05-12 22:00:07 +0545

    Update saral.py

--- saral.py
+++ saral.py
@@ -7,18 +7,19 @@ from fake_useragent import UserAgent
 def generate_random_numbers():
     return ''.join(str(random.randint(0, 9)) for _ in range(3))
 
-# Function to get token from server
-def get_token(session, headers):
+# Function to get token and payloads from server
+def get_token_and_payloads(session, headers):
     url = 'https://saralshikshya.com.np/register'
     try:
         response = session.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for non-2xx status codes
         soup = BeautifulSoup(response.content, 'html.parser')
         token = soup.find('input', {'name': '_token'}).get('value')
-        return token
+        payloads = soup.find_all('input')
+        return token, payloads
     except requests.exceptions.RequestException as e:
-        print(f"Failed to get token: {e}")
-        return None
+        print(f"Failed to get token and payloads: {e}")
+        return None, None
 
 # Function to submit form
 def submit_form(_token, session, _method, name, email, password, password_confirmation, referred_code, headers):
@@ -36,8 +37,11 @@ def submit_form(_token, session, _method, name, email, password, password_confir
         response = session.post(url, data=data, headers=headers)
         if 'https://saralshikshya.com.np/myprofile' in response.url:
             print("Form submitted successfully.")
-            print(email)
-            print(referred_code)
+            print("Email:", email)
+            print("Referred Code:", referred_code)
+            print("Payloads of Register Page:")
+            for payload in payloads:
+                print(payload)
         else:
             print("Form submission failed.")
     except Exception as e:
@@ -53,8 +57,8 @@ def main():
         headers = {"User-Agent": random.choice(user_agents)}
         for _ in range(2):  # Loop 5 times for testing, you can increase this number
             email = f'namastesaral{generate_random_numbers()}@gmail.com'
-            _token = get_token(session, headers)
-            if _token:
+            _token, payloads = get_token_and_payloads(session, headers)
+            if _token and payloads:
                 submit_form(_token, session, 'post', 'Namaste Haha', email, 'its_hack@123450', 'its_hack@123450', '655893', headers)
 
 if __name__ == "__main__":
